f5502a98c8e040b13875365545603ffd8ec14363
Tomisin Jenrola <tomisinjenrola@gmail.com>
2023-12-31 12:39:56 -0330

    add pro BottomBanner to /wrapped

--- frontend2/src/views/admin/nfts/collection/index.tsx
+++ frontend2/src/views/admin/nfts/collection/index.tsx
@@ -66,6 +66,7 @@ import MonthlyActivity from "./components/stats/charts/MonthlyActivity";
 
 
 import ReactGA from "react-ga4";
+import BottomBanner from "../marketplace/components/BottomBanner";
 
 
 const serverURL = process.env.REACT_APP_WEB3_WRAPPED_SERVER_URL;
@@ -158,132 +159,138 @@ export default function Collection() {
 
 	// Chakra Color Mode
 	return (
-		<Box pt={{ base: '180px', md: '80px', xl: '80px' }}>
-			{
-				address !== undefined && address.length === 42 ?
-				<>
-					<Text mt='25px' color={textColor} fontSize='2xl' ms='24px' fontWeight='700'>
-						Your year in web3
-					</Text>
-					<Text mb='36px' color={textColor} fontSize='l' ms='24px' fontWeight='700'>
-						More chains coming soon
-					</Text>
-					{stats &&
-						<Skeleton isLoaded={isLoaded}>
-							<SimpleGrid columns={{ base: 1, md: 2, xl: 3 }} gap='20px'>
-								<TopAccounts data={stats["top_accounts_sent_to"]} type="sent"/>
-								<TopAccounts data={stats["top_accounts_received_from"]} type="received"/>
-								<MonthlyActivity data={{
-									"monthly_transactions_sent": stats["monthly_transactions_sent"],
-									"monthly_transactions_received": stats["monthly_transactions_received"]
-								}} />
-								{/*<TransactionActivity />*/}
+		<>
+			<Box pt={{ base: '180px', md: '80px', xl: '80px' }}>
+				{
+					address !== undefined && address.length === 42 ?
+					<>
+						<Text mt='25px' color={textColor} fontSize='2xl' ms='24px' fontWeight='700'>
+							Your year in web3
+						</Text>
+						<Text mb='36px' color={textColor} fontSize='l' ms='24px' fontWeight='700'>
+							More chains coming soon
+						</Text>
+						{stats &&
+							<Skeleton isLoaded={isLoaded}>
+								<SimpleGrid columns={{ base: 1, md: 2, xl: 3 }} gap='20px'>
+									<TopAccounts data={stats["top_accounts_sent_to"]} type="sent"/>
+									<TopAccounts data={stats["top_accounts_received_from"]} type="received"/>
+									<MonthlyActivity data={{
+										"monthly_transactions_sent": stats["monthly_transactions_sent"],
+										"monthly_transactions_received": stats["monthly_transactions_received"]
+									}} />
+									{/*<TransactionActivity />*/}
 
-								<SimpleStat
-									data={stats["blocks_included_in"]}
-									type="blocks_included_in"
-									title={"You transactions were included in"}
-									focusTextUnit={"Blocks"}
-									backgroundImage={bgMastercard}
-								/>
-								<SimpleStat
-									data={stats["num_transactions"]}
-									type="num_transactions"
-									title={"You sent a total of"}
-									focusTextUnit={"Transactions"}
-									backgroundImage={Nft1}
-								/>
-								<SimpleStat
-									data={stats["largest_fee_paid"]}
-									type="largest_fee_paid"
-									title={"Your highest transaction fee was"}
-									focusTextUnit={"ETH"}
-									backgroundImage={NftBanner1}
-								/>
+									<SimpleStat
+										data={stats["blocks_included_in"]}
+										type="blocks_included_in"
+										title={"You transactions were included in"}
+										focusTextUnit={"Blocks"}
+										backgroundImage={bgMastercard}
+									/>
+									<SimpleStat
+										data={stats["num_transactions"]}
+										type="num_transactions"
+										title={"You sent a total of"}
+										focusTextUnit={"Transactions"}
+										backgroundImage={Nft1}
+									/>
+									<SimpleStat
+										data={stats["largest_fee_paid"]}
+										type="largest_fee_paid"
+										title={"Your highest transaction fee was"}
+										focusTextUnit={"ETH"}
+										backgroundImage={NftBanner1}
+									/>
 
-								{/*<LargestTransactionsTable*/}
-								{/*	txnType="sent"*/}
-								{/*	tableData={[*/}
-								{/*		{*/}
-								{/*			hash: '0xb6ac7b57ac9c43ba5237a781dbb0a8fe62fedbb241b472acfe2266b111ddcaf3',*/}
-								{/*			recipient: "0xbbdfc40fdee73cb6b60d9c88f25641bf111ab8e0",*/}
-								{/*			amount: "1.101219953608982636",*/}
-								{/*		},*/}
-								{/*		{*/}
-								{/*			hash: '0x6d6aa964cd1344ed0f2c4f5245f27398f629d46829e7fa8dafbe8c7acd0bc1d8',*/}
-								{/*			recipient: "0xe66b31678d6c16e9ebf358268a790b763c133750",*/}
-								{/*			amount: "0.920335241523553573",*/}
-								{/*		},*/}
-								{/*		{*/}
-								{/*			hash: '0xabae6494c6396fd9d79e702a4054e6dc0be7201aee5d58419e1c1b94cfac2562',*/}
-								{/*			recipient: "0xdef1c0ded9bec7f1a1670819833240f027b25eff",*/}
-								{/*			amount: "0.142204479699664130",*/}
-								{/*		}*/}
-								{/*	]}*/}
-								{/*/>*/}
-								{/*<LargestTransactionsTable*/}
-								{/*	txnType="received"*/}
-								{/*	tableData={[*/}
-								{/*		{*/}
-								{/*			hash: '0x6d6aa964cd1344ed0f2c4f5245f27398f629d46829e7fa8dafbe8c7acd0bc1d8',*/}
-								{/*			sender: "0xe66b31678d6c16e9ebf358268a790b763c133750",*/}
-								{/*			amount: "1.101219953608982636",*/}
-								{/*		},*/}
-								{/*		{*/}
-								{/*			hash: '0xabae6494c6396fd9d79e702a4054e6dc0be7201aee5d58419e1c1b94cfac2562',*/}
-								{/*			sender: "0xdef1c0ded9bec7f1a1670819833240f027b25eff",*/}
-								{/*			amount: "0.142204479699664130",*/}
-								{/*		},*/}
-								{/*		{*/}
-								{/*			hash: '0xaa1c4bfc29dea053b7a23e897e877abd5a5465b0e9192fbf93bd63fcaff2afd2',*/}
-								{/*			sender: "0xe66b31678d6c16e9ebf358268a790b763c133750",*/}
-								{/*			amount: "0.094803703664689830",*/}
-								{/*		}*/}
-								{/*	]}*/}
-								{/*/>*/}
-								{/*<SimpleStat*/}
-								{/*	data={stats["tokens_minted"]}*/}
-								{/*	type="tokens_minted"*/}
-								{/*	title={"Tokens you minted"}*/}
-								{/*	focusTextUnit={""}*/}
-								{/*	backgroundImage={Nft3}*/}
-								{/*/>*/}
-								{/*<MonthlyActivity data={{*/}
-								{/*	"monthly_transactions_sent": stats["monthly_transactions_sent"],*/}
-								{/*	"monthly_transactions_received": stats["monthly_transactions_received"]*/}
-								{/*}} />*/}
+									{/*<LargestTransactionsTable*/}
+									{/*	txnType="sent"*/}
+									{/*	tableData={[*/}
+									{/*		{*/}
+									{/*			hash: '0xb6ac7b57ac9c43ba5237a781dbb0a8fe62fedbb241b472acfe2266b111ddcaf3',*/}
+									{/*			recipient: "0xbbdfc40fdee73cb6b60d9c88f25641bf111ab8e0",*/}
+									{/*			amount: "1.101219953608982636",*/}
+									{/*		},*/}
+									{/*		{*/}
+									{/*			hash: '0x6d6aa964cd1344ed0f2c4f5245f27398f629d46829e7fa8dafbe8c7acd0bc1d8',*/}
+									{/*			recipient: "0xe66b31678d6c16e9ebf358268a790b763c133750",*/}
+									{/*			amount: "0.920335241523553573",*/}
+									{/*		},*/}
+									{/*		{*/}
+									{/*			hash: '0xabae6494c6396fd9d79e702a4054e6dc0be7201aee5d58419e1c1b94cfac2562',*/}
+									{/*			recipient: "0xdef1c0ded9bec7f1a1670819833240f027b25eff",*/}
+									{/*			amount: "0.142204479699664130",*/}
+									{/*		}*/}
+									{/*	]}*/}
+									{/*/>*/}
+									{/*<LargestTransactionsTable*/}
+									{/*	txnType="received"*/}
+									{/*	tableData={[*/}
+									{/*		{*/}
+									{/*			hash: '0x6d6aa964cd1344ed0f2c4f5245f27398f629d46829e7fa8dafbe8c7acd0bc1d8',*/}
+									{/*			sender: "0xe66b31678d6c16e9ebf358268a790b763c133750",*/}
+									{/*			amount: "1.101219953608982636",*/}
+									{/*		},*/}
+									{/*		{*/}
+									{/*			hash: '0xabae6494c6396fd9d79e702a4054e6dc0be7201aee5d58419e1c1b94cfac2562',*/}
+									{/*			sender: "0xdef1c0ded9bec7f1a1670819833240f027b25eff",*/}
+									{/*			amount: "0.142204479699664130",*/}
+									{/*		},*/}
+									{/*		{*/}
+									{/*			hash: '0xaa1c4bfc29dea053b7a23e897e877abd5a5465b0e9192fbf93bd63fcaff2afd2',*/}
+									{/*			sender: "0xe66b31678d6c16e9ebf358268a790b763c133750",*/}
+									{/*			amount: "0.094803703664689830",*/}
+									{/*		}*/}
+									{/*	]}*/}
+									{/*/>*/}
+									{/*<SimpleStat*/}
+									{/*	data={stats["tokens_minted"]}*/}
+									{/*	type="tokens_minted"*/}
+									{/*	title={"Tokens you minted"}*/}
+									{/*	focusTextUnit={""}*/}
+									{/*	backgroundImage={Nft3}*/}
+									{/*/>*/}
+									{/*<MonthlyActivity data={{*/}
+									{/*	"monthly_transactions_sent": stats["monthly_transactions_sent"],*/}
+									{/*	"monthly_transactions_received": stats["monthly_transactions_received"]*/}
+									{/*}} />*/}
 
-								<SimpleStat
-									data={stats["tokens_burnt"]}
-									type="tokens_burnt"
-									title={"Tokens you burnt"}
-									focusTextUnit={""}
-									backgroundImage={Nft4}
-								/>
-								<SimpleStat
-									data={stats["nfts_minted"]}
-									type="nfts_minted"
-									title={"NFTs you minted"}
-									focusTextUnit={""}
-									backgroundImage={Nft5}
-								/>
-								<SimpleStat
-									data={stats["nfts_burnt"]}
-									type="nfts_burnt"
-									title={"NFTs you burnt"}
-									focusTextUnit={""}
-									backgroundImage={Nft6}
-								/>
-							</SimpleGrid>
-						</Skeleton>
-					}
-				</>
-				:
-				<>
-					<Text mb='36px' ms='24px'>Please connect your wallet to get started.</Text>
-					<Sample />
-				</>
-			}
-		</Box>
+									<SimpleStat
+										data={stats["tokens_burnt"]}
+										type="tokens_burnt"
+										title={"Tokens you burnt"}
+										focusTextUnit={""}
+										backgroundImage={Nft4}
+									/>
+									<SimpleStat
+										data={stats["nfts_minted"]}
+										type="nfts_minted"
+										title={"NFTs you minted"}
+										focusTextUnit={""}
+										backgroundImage={Nft5}
+									/>
+									<SimpleStat
+										data={stats["nfts_burnt"]}
+										type="nfts_burnt"
+										title={"NFTs you burnt"}
+										focusTextUnit={""}
+										backgroundImage={Nft6}
+									/>
+								</SimpleGrid>
+							</Skeleton>
+						}
+					</>
+					:
+					<>
+						<Text mb='36px' ms='24px'>Please connect your wallet to get started.</Text>
+						<Sample />
+					</>
+				}
+			</Box>
+
+			<Box mt={4}>
+				<BottomBanner/>
+			</Box>
+		</>
 	);
 }
